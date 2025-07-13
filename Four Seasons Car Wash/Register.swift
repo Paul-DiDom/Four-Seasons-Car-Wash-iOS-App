c112eede36fa91802b008a8c4d044406ef96fe61
Paul-DiDom <pauldidomencio@icloud.com>
2025-07-13 17:43:47 -0400

    Updated July 13 2025 543p

--- Four Seasons Car Wash/Register.swift
+++ Four Seasons Car Wash/Register.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Foundation
+import Foundation
+import FirebaseAuth
 
 class Register: UIViewController, UITextFieldDelegate {
     
@@ -18,6 +20,7 @@ class Register: UIViewController, UITextFieldDelegate {
         
         password.delegate = self
         confirmPassword.delegate = self
+        checkFreeCodeOffer()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -58,7 +61,7 @@ class Register: UIViewController, UITextFieldDelegate {
             showIt(title: "", msg: "Please enter Last Name")
             return
         }
-        else if (!isValidPhone(number: fixedPhoneNum)){
+        if (!fixedPhoneNum.isEmpty && !isValidPhone(number: fixedPhoneNum)) {
             showIt(title: "", msg: "Please enter a valid phone number")
             return
         }
@@ -79,7 +82,160 @@ class Register: UIViewController, UITextFieldDelegate {
             return
         }
         
-        registerUser("empty", firstName: firstNameText, lastName: lastNameText, email: emailText, password: passwordText, phoneNumber: fixedPhoneNum)
+        var jsonBody: [String: Any]
+
+        if (fixedPhoneNum.count > 8) {
+            jsonBody = [
+                "firstName": firstNameText,
+                "lastName": lastNameText,
+                "phone": fixedPhoneNum,
+                "email": emailText,
+                "password": passwordText,
+                "l": getSite()
+            ]
+        } else {
+            jsonBody = [
+                "firstName": firstNameText,
+                "lastName": lastNameText,
+                "email": emailText,
+                "password": passwordText,
+                "l": getSite()
+            ]
+        }
+        
+        let urlString = service + "rver2"
+        var request = URLRequest(url: URL(string: urlString)!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONSerialization.data(withJSONObject: jsonBody)
+        
+        pleaseWait()
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            defer { DispatchQueue.main.async { self.endWait() } }
+            
+            if let error = error {
+                DispatchQueue.main.async {
+                    self.showIt(title: "Oops!", msg: error.localizedDescription)
+                }
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  httpResponse.statusCode == 200,
+                  let data = data else {
+                DispatchQueue.main.async {
+                    self.showIt(title: "Oops!", msg: "Server error. Please try again.")
+                }
+                return
+            }
+            
+            do {
+                // Parse outer response
+                guard
+                    let outer = try JSONSerialization.jsonObject(with: data) as? [String: Any],
+                    let outerString = outer["rver2Result"] as? String,
+                    let innerData = outerString.data(using: .utf8),
+                    let inner = try JSONSerialization.jsonObject(with: innerData) as? [String: Any]
+                else {
+                    DispatchQueue.main.async {
+                        self.showIt(title: "Oops!", msg: "Unexpected response format.")
+                    }
+                    return
+                }
+                
+                if myDebug {
+                    print("register() result =", inner)
+                }
+                
+                let success = (inner["success"] as? String)?.lowercased() == "true"
+                self.endWait()
+                if success {
+                    let userId = inner["localId"] as? String ?? ""
+                    UserDefaults.standard.set(true, forKey: "freeCodeGiven")
+                    self.logIn(email: emailText, password: passwordText)
+                    
+                } else {
+                    let errorCode = inner["error"] as? String ?? ""
+                    let message = Self.friendlyMessage(for: errorCode)
+                    DispatchQueue.main.async {
+                        self.showIt(title: "Registration Failed", msg: message)
+                    }
+                }
+                
+            } catch {
+                DispatchQueue.main.async {
+                    self.showIt(title: "Oops!", msg: "Failed to parse server response.")
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func checkFreeCodeOffer() {
+        if UserDefaults.standard.bool(forKey: "freeCodeGiven") {
+            return
+        }
+
+        guard let url = URL(string: service + "f") else { return }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONSerialization.data(withJSONObject: [:], options: [])
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil, let data = data else { return }
+
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data) as? [String: Any],
+                   let result = json["fResult"] as? String,
+                   result == "1" {
+                    DispatchQueue.main.async {
+                        self.showFreeCodeMessage()
+                    }
+                }
+            } catch {
+                if (myDebug){
+                    self.debugPrint("checkFreeCodeOffer()")
+                }
+            }
+        }
+        task.resume()
+    }
+
+    
+    func showFreeCodeMessage() {
+        let alert = UIAlertController(
+            title: "✅ Try our app for FREE!",
+            message: """
+            To receive a FREE $4 wash code for use in the COIN BAYS, please enter your phone number. This will also allow you to receive alerts about promotions.
+
+            You may register without a phone number if you prefer.
+            """,
+            preferredStyle: .alert
+        )
+
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    
+    /// Convert technical error codes to user-friendly text
+    private static func friendlyMessage(for code: String) -> String {
+        switch code {
+        case "EMAIL_EXISTS":
+            return "An account with this email already exists. Please log in or use another email."
+        case "WEAK_PASSWORD":
+            return "Password is too weak. Please choose a stronger password."
+        case "INVALID_EMAIL":
+            return "Please enter a valid email address."
+        case "TOO_MANY_ATTEMPTS_TRY_LATER":
+            return "Too many attempts. Please try again later."
+        default:
+            return "Registration failed. Please check your information and try again."
+        }
         
     }
     
@@ -94,71 +250,12 @@ class Register: UIViewController, UITextFieldDelegate {
         
         self.view.layer.insertSublayer(gradientLayer, at:0)
     }
-    
-//    @IBAction func btnRegisterClicked(_ sender: Any) {
-//        if (getSite() != 0)
-//        {
-//            if (checkNet()) {
-//                if (UserDefaults.standard.object(forKey: "freeCode") != nil)
-//                {
-//                    gotFreeCode = UserDefaults.standard.object(forKey: "freeCode") as! Bool
-//                }
-//                if (gotFreeCode) {
-//                    register()
-//                }
-//                else {
-//                    pleaseWait()
-//                    let myUrl = URL(string: service + "f")!
-//                    var request = URLRequest(url:myUrl);
-//                    request.httpMethod = "POST";
-//                    request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
-//                    let dictionary = [String: String]()
-//                    request.httpBody = try! JSONSerialization.data(withJSONObject: dictionary, options: [])
-//                    let task = URLSession.shared.dataTask(with: request, completionHandler: {
-//                        data, response, error in
-//                        
-//                        if error != nil
-//                        {
-//                            return
-//                        }
-//                        
-//                        do {
-//                            let myJSON = try JSONSerialization.jsonObject(with: data!, options: .mutableLeaves) as? NSDictionary
-//                            if let parseJSON = myJSON {
-//                                let myResult = parseJSON["fResult"] as? String
-//                                print(myResult!)
-//                                if (myResult != "1")
-//                                {
-//                                    self.giveFreeCode = false
-//                                    DispatchQueue.main.async(execute: { () -> Void in
-//                                        self.endWait()
-//                                        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.register), userInfo: nil, repeats: false)
-//                                        
-//                                    })
-//                                }
-//                                else {
-//                                    self.giveFreeCode = true
-//                                    DispatchQueue.main.async(execute: { () -> Void in
-//                                        self.endWait()
-//                                        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.register), userInfo: nil, repeats: false)
-//                                    })
-//                                }
-//                            }
-//                        }
-//                        catch {
-//                            self.endWait()
-//                            Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.register), userInfo: nil, repeats: false)                    }
-//                    })
-//                    task.resume()
-//                }
-//            }
-//        }
-//    }
-    
+
 }
 
 extension Register {
-   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-      textField.resignFirstResponder()
-   }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
